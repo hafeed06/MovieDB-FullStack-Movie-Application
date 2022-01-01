2d09cf60b693a02d2d04e158fdf4cec98afabcbe
Hafeed <Hafeed06@gmail.com>
2022-01-01 20:16:29 +0100

    Initiation of the Java ServerSide with SpringBoot

---
+++ severjava/moviedb/src/main/java/co/hafid/moviedb/MoviedbApplication.java
@@ -0,0 +1,13 @@
+package co.hafid.moviedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviedbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviedbApplication.class, args);
+    }
+
+}
---
+++ severjava/moviedb/src/test/java/co/hafid/moviedb/MoviedbApplicationTests.java
@@ -0,0 +1,13 @@
+package co.hafid.moviedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviedbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
